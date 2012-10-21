a8933de1cdde95c914fb26022ac9026c3acab703
Larry Price <laprice@gmail.com>
2012-10-20 17:26:37 -0700

    Added protection for the dreaded Pop from and empty list error.

--- target/spiders/dailydeal_spider.py
+++ target/spiders/dailydeal_spider.py
@@ -12,21 +12,27 @@ class TargetSpider(BaseSpider):
         deals = []
         fdiv = hxs.select("//div[@id='dealsContent']")
         for d in fdiv:
-            t = TargetItem()
-            t['link'] = d.select(".//p[@class='siteLink']/a/@href").extract().pop()
-            t['product'] = d.select(".//p[@class='siteLink']/a/text()").extract().pop()
-            t['price'] = d.select(".//div[@class='dealPrice']/p/text()"
-                                  ).extract().pop().strip()
-            t['list_price'] = d.select(".//p[contains(@class,'lstPrice')]/del/text()"
-                                       ).extract().pop().strip()
-            deals.append(t)
+            try:
+                t = TargetItem()
+                t['link'] = d.select(".//p[@class='siteLink']/a/@href").extract().pop()
+                t['product'] = d.select(".//p[@class='siteLink']/a/text()").extract().pop()
+                t['price'] = d.select(".//div[@class='dealPrice']/p/text()"
+                                      ).extract().pop().strip()
+                t['list_price'] = d.select(".//p[contains(@class,'lstPrice')]/del/text()"
+                                           ).extract().pop().strip()
+                deals.append(t)
+            except IndexError:
+                continue
         tiles = hxs.select("//div[@class='tileInfo']")
         for d in tiles:
-            t = TargetItem()
-            t['link'] = d.select(".//span[@class='productTitle']/a/@href").extract().pop()
-            t['product'] = d.select(".//span[@class='productTitle']/a/@title").extract().pop()
-            t['price'] = d.select(".//p[@class='price']/text()").extract().pop().strip()
-            t['list_price'] = d.select(".//p[contains(@class,'lstPrice')]/del/text()"
-                                       ).extract().pop().strip()
-            deals.append(t)
+            try:
+                t = TargetItem()
+                t['link'] = d.select(".//span[@class='productTitle']/a/@href").extract().pop()
+                t['product'] = d.select(".//span[@class='productTitle']/a/@title").extract().pop()
+                t['price'] = d.select(".//p[@class='price']/text()").extract().pop().strip()
+                t['list_price'] = d.select(".//p[contains(@class,'lstPrice')]/del/text()"
+                                           ).extract().pop().strip()
+                deals.append(t)
+            except IndexError:
+                continue
         return deals
