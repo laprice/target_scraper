280cc8d2eae422c998847d87a57f2c2f6b8cfabc
Larry Price <laprice@gmail.com>
2012-10-20 17:13:08 -0700

    Now produces clean, if brittle items.

--- target/spiders/dailydeal_spider.py
+++ target/spiders/dailydeal_spider.py
@@ -13,17 +13,18 @@ class TargetSpider(BaseSpider):
         fdiv = hxs.select("//div[@id='dealsContent']")
         for d in fdiv:
             t = TargetItem()
-            t['link'] = d.select(".//p[@class='siteLink']/a/@href").extract()
-            t['product'] = d.select(".//p[@class='siteLink']/a/text()").extract()
-            t['price'] = d.select(".//div[@class='dealPrice']/p/text()").extract()
-            t['list_price'] = d.select(".//p[@class='lstPrice']/del/text()"
-                                       ).extract()
+            t['link'] = d.select(".//p[@class='siteLink']/a/@href").extract().pop()
+            t['product'] = d.select(".//p[@class='siteLink']/a/text()").extract().pop()
+            t['price'] = d.select(".//div[@class='dealPrice']/p/text()"
+                                  ).extract().pop().strip()
+            t['list_price'] = d.select(".//p[contains(@class,'lstPrice')]/del/text()"
+                                       ).extract().pop().strip()
             deals.append(t)
         tiles = hxs.select("//div[@class='tileInfo']")
         for d in tiles:
             t = TargetItem()
-            t['link'] = d.select(".//span[@class='productTitle']/a/@href").extract()
-            t['product'] = d.select(".//span[@class='productTitle']/a/@title").extract()
+            t['link'] = d.select(".//span[@class='productTitle']/a/@href").extract().pop()
+            t['product'] = d.select(".//span[@class='productTitle']/a/@title").extract().pop()
             t['price'] = d.select(".//p[@class='price']/text()").extract().pop().strip()
             t['list_price'] = d.select(".//p[contains(@class,'lstPrice')]/del/text()"
                                        ).extract().pop().strip()
